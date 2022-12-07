a80bb8f307d734288bd2893ed74961b845cc5295
Mateus Meneses Rocha <menesescriativa@gmail.com>
2022-12-07 17:01:15 -0300

    Construção do Layout pág. de adição de itens

--- lib/paginas/item_add_page.dart
+++ lib/paginas/item_add_page.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 
 class ItemAddPage extends StatelessWidget {
@@ -6,8 +8,28 @@ class ItemAddPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-      title: Text('Primeiro Item'),
-    ));
+      appBar: AppBar(
+        title: Text('Novo Item'),
+      ),
+      floatingActionButton: FloatingActionButton(
+          backgroundColor: Colors.yellow,
+          child: Icon(Icons.done),
+          onPressed: () {
+            Navigator.pop(context);
+          }),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.only(top: 20, left: 20),
+            child: Text('Descrição do Item'),
+          ),
+          Padding(
+            padding: EdgeInsets.only(right: 20, left: 20),
+            child: TextField(),
+          ),
+        ],
+      ),
+    );
   }
 }
