eb9062d97a66341bbb2c6879132d23d8ad0a5376
Mateus Meneses Rocha <menesescriativa@gmail.com>
2022-12-08 16:04:24 -0300

    Lista de itens e adição de itens agora funcionais

--- lib/paginas/home.dart
+++ lib/paginas/home.dart
@@ -12,29 +12,42 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  String texto = '';
+  List<String> _list = [];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('@meneses.dev - frist app'),
-      ),
-      
-      floatingActionButton: FloatingActionButton(
-        onPressed: () async {
-          final result = await Navigator.push(context,
-              MaterialPageRoute<String>(builder: (context) {
-            return ItemAddPage();
-          }));
-          setState(() {
-            texto = result!;
-          });
-        },
-        backgroundColor: Colors.white,
-        child: Icon(Icons.arrow_right),
-      ),
-      body: Center(child: Text(texto)),
-    );
+        appBar: AppBar(
+          title: Text('@meneses.dev - frist app'),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () async {
+            final result = await Navigator.push(context,
+                MaterialPageRoute<String>(builder: (context) {
+              return ItemAddPage();
+            }));
+            setState(() {
+              if (result!.isNotEmpty) {
+                _list.add(result!);
+              }
+            });
+          },
+          backgroundColor: Colors.white,
+          child: Icon(Icons.arrow_right),
+        ),
+        body: _list.length > 0
+            ? ListView.builder(
+                itemCount: _list.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return Card(
+                    child: ListTile(
+                      title: Text(_list[index]),
+                    ),
+                  );
+                },
+              )
+            : Center(
+                child: Text('Não há itens'),
+              ));
   }
 }
